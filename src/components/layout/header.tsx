cd87f382341ebc0879baa0c3d66aa7b6f251d8d2
haloapping <alfiyanto.kondolele@gmail.com>
2025-01-29 23:01:16 +0700

    fix: delete unused component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,54 +1,5 @@
 import { AddTaskDialog } from "@/components/tasks/add-task";
 import { Task } from "@/types/task";
-import { NavLink } from "react-router";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-
-export function NavBar() {
-  return (
-    <header className="mx-auto mb-5 mt-10 w-3/5">
-      <div className="flex flex-col">
-        <div className="alig mb-5 flex justify-between">
-          <section className="flex items-center justify-between gap-1">
-            <a href="">
-              <img src="/logo.svg" alt="logo" width={30} />
-            </a>
-            <a href="/">
-              <h1>KlarinTask</h1>
-            </a>
-          </section>
-
-          <section className="flex items-center justify-between gap-1">
-            <nav className="mr-8 flex gap-4">
-              <NavLink
-                to={"/"}
-                className={({ isActive }) =>
-                  isActive ? "font-bold" : "hover:underline"
-                }
-              >
-                Home
-              </NavLink>
-              <NavLink
-                to={"/about"}
-                className={({ isActive }) =>
-                  isActive ? "font-bold" : "hover:underline"
-                }
-              >
-                About
-              </NavLink>
-            </nav>
-
-            <Avatar>
-              <AvatarImage src="/user-avatar.jpg" alt="avatar" />
-              <AvatarFallback>FU</AvatarFallback>
-            </Avatar>
-
-            <p>FafaFufu</p>
-          </section>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 type HeaderProps = {
   username: string;
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 
 import { Footer } from "@/components/layout/footer";
-import { NavBar } from "@/components/layout/header";
+import { NavBar } from "@/components/layout/navbar";
 
 export function LayoutRoute() {
   return (
